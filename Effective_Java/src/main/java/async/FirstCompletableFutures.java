3ee7c4b5557734d50574d38288ac20f35f528b56
Deven Kalra <deven@Macbook.local>
2024-01-06 10:51:44 +0530

    Added Files Through Upload

---
+++ Effective_Java/src/main/java/async/FirstCompletableFutures.java
@@ -0,0 +1,23 @@
+package async;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FirstCompletableFutures {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		
+		Runnable task = () -> {
+			System.out.println("I am running asynchronously in the thread "
+					+ Thread.currentThread().getName()+":"+Thread.currentThread().getState().name());
+		};
+		
+		CompletableFuture.runAsync(task);
+		
+		Thread.sleep(100);
+		executor.shutdown();
+	}
+}
---
+++ Effective_Java/src/main/java/com/cleo/async/Shop.java
@@ -0,0 +1,16 @@
+package com.cleo.async;
+
+
+import org.w3c.dom.events.Event;
+import sun.misc.Signal;
+
+import java.util.UUID;
+
+public class Shop {
+
+    private UUID id;
+    private String name;
+
+
+
+}
