b44a3efe5de6a3a6b6f6e49ff59e112b456bf9aa
Deven Kalra <deven@Macbook.local>
2024-01-13 04:10:43 +0530

    Added Files Through Upload

--- LLD/src/main/java/com/cleo/best_price_finder/Main.java
+++ LLD/src/main/java/com/cleo/best_price_finder/Main.java
@@ -1,5 +1,25 @@
 package com.cleo.best_price_finder;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 public class Main {
 
+    private static final String PRODUCT = "iPhone 15 Plus";
+    private static BestPriceCalculator calculator = new BestPriceCalculator();
+
+    public static void main(String[] args) {
+        execute("Sequential: " ,  ()->calculator.findPricesSequential(PRODUCT));
+        execute("Parallel: " ,  ()->calculator.findPricesParallel(PRODUCT));
+        execute("Asynchronous Implementation: " ,  ()->calculator.findPricesAsync(PRODUCT));
+
+
+    }
+
+    private static void execute(String msg, Supplier<List<String>> s){
+        long start = System.nanoTime();
+        System.out.println(s.get());
+        long duration = (System.nanoTime() - start) / 1_000_000;
+        System.out.println(msg + " done in " + duration + " msecs");
+    }
 }
